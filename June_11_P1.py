f1ff05456d3ed51452df51cbac7cf7345bc955a0
jamesdeal89 <j891319@protonmail.com>
2022-05-28 13:18:07 +0800

    added June 2011 paper Q

---
+++ June_11_P1.py
@@ -0,0 +1,16 @@
+# code for Q7 June 2011 CS Paper 1 CIE
+
+names = ["Ben", "Thor", "Zoe", "Kate"]
+current = 0
+found = False
+playerName = input("which player are you looking for?")
+while found == False and current+1 <= len(names):
+    if names[current] == playerName:
+        found = True
+    else:
+        current += 1
+if found == True:
+    print("Yes, they do have a top score")
+else:
+    print("No, they do not have a top score")
+
